759cb1a8e92c9255a1f8f1fc5f695e82a99b326c
quimaruanomed <jruanomed@gmail.com>
2020-11-06 18:46:14 +0100

    first commit

---
+++ M2.2_FaseI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace M2._2_FaseI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] nameLetters = { 'j', 'o', 'a', 'q', 'u', 'i', 'n', 'a' };
+            for (int i=0; i<=nameLetters.Length;i++)  //Recorrerá el array nameLetters
+            {
+                Console.WriteLine(nameLetters[i]); //Imprime cada posición del array,la posición se irá incrementando hasta llegar a la ultima letra
+                
+
+
+            }
+
+        }
+    }
+}
